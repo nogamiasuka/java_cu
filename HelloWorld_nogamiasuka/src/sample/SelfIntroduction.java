bf35adef2d7380c9bafe7fe32adc844221d9648a
野上明日香 <asu0704gool5066@gmail.com>
2023-06-20 07:10:41 +0900

    課題1.3　新規作成

---
+++ HelloWorld_nogamiasuka/src/sample/SelfIntroduction.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class SelfIntroduction {
+	public static void main(String[] args){
+		// 氏名
+		String name = "野上 明日香";
+		// 年齢
+		int age = 20;
+		// 趣味
+		String hobby = "絵を描くこと";
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		}
+}
